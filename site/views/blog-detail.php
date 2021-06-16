4419a844642c04f1d1b75c43cea46a315e8e5b92
longnguyen24101 <73817950+longnguyen24101@users.noreply.github.com>
2021-06-16 23:21:20 +0700

    them trang blog detail

--- site/views/blog-detail.php
+++ site/views/blog-detail.php
@@ -1,33 +1,39 @@
 <section class="ftco-section bg-light">
     <div class="container">
         <div class="row">
-            <div class="col-lg-9">   
-                <div class="image-blog">
-                    <div class="photo" style="background-image: url(<?=PATH_URL?>images/Patrick_painter-600x400.jpg)">
-                </div>             
-                <div class="row">
-                    <div class="col-6">
-                        <h1>Hội thảo “Phương án dạy và học tiếng Anh hiệu quả, chất lượng”</h1>
-
-                    </div>
-                </div>
-                
-                <div class="row mt-5">
-                    <div class="col">
-                        <div class="block-27">
-                            <ul>
-                                <li><a href="#">&lt;</a></li>
-                                <li class="active"><span>1</span></li>
-                                <li><a href="#">2</a></li>
-                                <li><a href="#">3</a></li>
-                                <li><a href="#">4</a></li>
-                                <li><a href="#">5</a></li>
-                                <li><a href="#">&gt;</a></li>
-                            </ul>
+            <div class="col-lg-9">
+                <div class="blog-detail">
+                    <div class="blog-header">
+                        <div class="col-md-12 heading-section text-left ftco-animate fadeInUp ftco-animated">
+                            <h3 class="text-uppercase">HỆ THỐNG SÁCH TIẾNG ANH THEO CẤP HỌC</h3>
+                            <small>28/05/2021</small>
                         </div>
+                    </div>
+                    <div class="blog-content col-md-12 mt-4">
+                        <p>
+                        <b>Sách giáo khoa Tiếng Anh 6</b> nằm trong bộ sách giáo khoa theo Chương trình
+                        giáo dục phổ thông 2018 do GS.TS. Hoàng Văn Vân là Tổng Chủ biên là sản
+                        phẩm của sự hợp tác chặt chẽ về chuyên môn và nghiệp vụ giữa Nhà xuất bản 
+                        Giáo dục Việt Nam với Nhà xuất bản Pearson Education ở bậc học Trung học 
+                        cơ sở và Trung học phổ thông. Nội dung sách bám sát Chương trình giáo dục 
+                        phổ thông môn tiếng Anh và Khung năng lực ngoại ngữ 6 bậc dùng cho Việt Nam 
+                        của Bộ Giáo dục và Đào tạo, đảm bảo tính liên thông từ lớp 1 đến lớp 12.
+                        </p>
+                        <div class="w-100 text-center mt-5 mb-5">
+                            <img src="<?=PATH_URL?>images/Patrick_painter-600x400.jpg" class="img-fluid mr-0-auto" alt="">
+                        </div>
+                        <p>
+                        &nbsp;<b>Sách giáo khoa Tiếng Anh 6</b> nằm trong bộ sách giáo khoa theo Chương trình
+                        giáo dục phổ thông 2018 do GS.TS. Hoàng Văn Vân là Tổng Chủ biên là sản
+                        phẩm của sự hợp tác chặt chẽ về chuyên môn và nghiệp vụ giữa Nhà xuất bản 
+                        Giáo dục Việt Nam với Nhà xuất bản Pearson Education ở bậc học Trung học 
+                        cơ sở và Trung học phổ thông. Nội dung sách bám sát Chương trình giáo dục 
+                        phổ thông môn tiếng Anh và Khung năng lực ngoại ngữ 6 bậc dùng cho Việt Nam 
+                        của Bộ Giáo dục và Đào tạo, đảm bảo tính liên thông từ lớp 1 đến lớp 12.
+                        </p>
                     </div>
                 </div>
             </div>
-            <?php require 'right.php'; ?>
+            <?php require 'right.php';?>
         </div>
 </section>
